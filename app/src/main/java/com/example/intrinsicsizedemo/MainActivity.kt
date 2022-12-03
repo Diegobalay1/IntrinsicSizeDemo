79c8796397157a7bf85a3e473bdccbbb0ff7265c
balay <dlolhd@gmail.com>
2022-12-03 17:01:44 +0100

    Añadiendo los componentes Text y Box.

--- app/src/main/java/com/example/intrinsicsizedemo/MainActivity.kt
+++ app/src/main/java/com/example/intrinsicsizedemo/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.intrinsicsizedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,6 +15,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.intrinsicsizedemo.ui.theme.IntrinsicSizeDemoTheme
 
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +40,15 @@ fun MainScreen() {
     var textState by remember { mutableStateOf("") }
     val onTextChange = { text: String ->
         textState = text
+    }
+
+    Column {
+        Text(
+            text = textState,
+            modifier = Modifier
+                .padding(start = 4.dp)
+        )
+        Box(modifier = Modifier.height(10.dp).fillMaxWidth().background(Color.Blue))
     }
 }
 
